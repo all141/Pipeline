3f3e3e622a9cfd0e037f8dd73887661ecec50477
all141 <all141@pitt.edu>
2018-02-18 15:11:09 -0500

    Update CPU.c

--- CPU.c
+++ CPU.c
@@ -15,6 +15,7 @@ struct trace_item buff_stages[8]; //Allocation of memory for instructions
 int stall_flag = 0;				  //Tells push_pipline() to stall
 int prediction_method = 0;		  //Defaults to 0
 int squash_flag = 0;
+int squash_print = 2;
 
 /* Function: init_pipeline
  * ------------------------
@@ -83,7 +84,7 @@ void check_hazards(struct trace_item entry){
 		if(buff_stages[1].PC == buff_stages[2].Addr){
 			printf("CONTROL HAZARD\nPC of [1]: (%x), PC of EX/MEM1 : (%x)\n", buff_stages[1].PC, buff_stages[3].Addr);
 			squash_flag = 3;
-			
+			squash_print = 5;
 			/*buff_stages[0].type = ti_NOP;
 			buff_stages[1].type = ti_NOP;
 			entry.type = ti_NOP;
@@ -166,6 +167,173 @@ void push_pipeline(struct trace_item entry){
 	//check hazards here again?
 }
 
+//BTB**************************************************************************************************
+/**
+* Object in the BTB
+* Holds a one of two bit branch predictor, the pc that the branch evaluates to and the PC of the branch instruction
+*/
+struct BranchEntry {
+	int prediction;
+	char targetAddr[6];
+	char branchPC[6];
+};
+
+//Get the index to look up in the BTB using the address
+int getIndex(int address){
+	int a = address;
+	a = a % 1024;//2048, 512 for different branch table sizes ALSO get final 8 bits
+	a = a / 8; //Get bits 8-3
+	a = a % 64; //INDEX
+	return a;
+}
+
+//Convert Hexadecimal string address to a decimal to operate on
+int hexToDec(char *address){
+        unsigned char decAddress = 0;
+	    decAddress = (unsigned char) strtoul(address, NULL, 16); //BASE 16 FOR HEX VALUES
+	return decAddress;
+}
+
+//See what the current prediciton is of an entry in the BTB
+int checkPrediction(int s, int p){
+	if(s == 2){
+		if(p==0 || p==1){
+			return 0;//branch not taken
+		}else if(p==2 || p==3){
+			return 1; //branch taken
+		}else{
+			printf("INVALID p");
+			system("exit");
+			}
+	}else if(s == 1){
+		return p;
+	}else{
+		printf("INVALID PREDICTION STYLE");
+		system("exit");
+		}
+}
+
+//P = prediction s = prediction style given by starting arguments.
+//If hitMiss = 1 then update to hit else update to miss
+int updatePrediction(int p, int s, int hitMiss)
+{
+	switch(s)
+	{
+		case 1://One Bit Predictor
+		if(hitMiss==0) {
+			return 0;
+		}else if(hitMiss==1) {
+			return 1;
+		}else{
+			printf("Invalid hitMiss!");
+			system("exit");
+		}
+		break;
+		case 2: //Two Bit Predictor
+			switch(p){
+				case 0://Predict Not Taken 00
+					if(hitMiss == 0){
+						return 0;//Predicition is 00
+					}else if(hitMiss == 1){
+						return 1; //Prediction is 01
+					}else{
+						printf("Invalid hitMiss");
+						system("exit");
+					}	
+				break;
+
+				case 1://Predict Not Taken 01
+					if(hitMiss == 0){
+						return 0;//Prediction is 00
+					}else if(hitMiss == 1){
+						return 3; //Prediction is 11
+					}else{
+						printf("Invalid hitMiss");
+						system("exit");
+					}
+				break;
+
+				case 2: //Predict Taken 10
+					if(hitMiss == 0){
+						return 0;//Prediciton is 00
+					}else if(hitMiss == 1){
+						return 3; //Prediction is 11
+					}else{
+						printf("Invalid hitMiss");
+						system("exit");
+					}
+				break;
+				case 3:
+					if(hitMiss == 0){
+						return 2; //Prediction is 10
+					}else if(hitMiss == 1){
+						return 3; //Prediction is 11
+					}else{
+						printf("Invalid hitMiss");
+						system("exit");
+					}	
+				break;
+
+				default:
+					printf("Invalid Prediction!");
+					system("exit");
+			}
+		break;
+		default:
+			printf("Invalid case!");
+			system("exit");
+	}
+}
+int branch_prediction(struct trace_item tr_entry)
+{
+	int currentPC = tr_entry->PC;
+	int entryIndex = getIndex(currentPC); 
+	if(BranchTable[entryIndex]==null)
+	{//No prediction available
+		if(buff_stages[2].type == ti_BRANCH)
+		{//Is instruction a branch
+			//Instruction is a branch
+			if(tr_entry->PC == buff_stages[2].Addr) 
+			{//Is the branch taken
+				//Correct the PC;
+				tr_entry->type = ti_NOP;
+				buff_stages[0].type = ti_NOP
+				BranchTable[entryindex].prediction = updatePrediction(BranchTable[entryIndex].prediction, ARGV[2], 1);
+				BranchTable[entryindex].targetAddr = buff_stages[2].Addr;
+				BranchTable[entryindex].branchPC = buff_stages[2].PC;
+				//SQUASH
+			}
+			else 
+			{
+				BranchTable[entryindex].prediction = updatePrediction(BranchTable[entryIndex].prediction, ARGV[2], 0);
+				BranchTable[entryindex].targetAddr = buff_stages[2].Addr;
+				BranchTable[entryindex].branchPC = buff_stages[2].PC;
+			}
+		}
+		else 
+		{//Instruction is not a branch
+			break;
+	else{//Prediction is in BTB
+		currentPC = BranchTable[entryIndex].branchPC;
+		if(buff_stages[2].Addr == buff_stages[1].PC) //Was prediction correct
+		{
+			break;
+		}
+		else	
+		{
+			tr_entry->type = ti_NOP
+			buff_stages[0].type = ti_NOP
+			BranchTable[entryindex].prediction = updatePrediction(BranchTable[entryIndex].prediction, ARGV[2], 1);
+			BranchTable[entryindex].targetAddr = buff_stages[2].Addr;
+			BranchTable[entryindex].branchPC = buff_stages[2].PC;
+			//SQUASH
+		}
+		}
+	}
+}
+
+
+//BEGIN EXECUTION********************************************************************
 int main(int argc, char **argv)
 {
   
@@ -175,6 +343,9 @@ int main(int argc, char **argv)
   int trace_view_on = 0;
   int empty_flag = 0;
   int empty_count = 0;
+  
+//BTB is an array of Branch entry structs
+struct BranchEntry BranchTable[64];
   
   unsigned char t_type = 0;
   unsigned char t_sReg_a= 0;
@@ -214,7 +385,7 @@ int main(int argc, char **argv)
   
   //Loop until the end of the trace file
   while(1) {
-	if((stall_flag == 0) || (empty_flag != 0)){
+	if((stall_flag == 0) || (empty_flag != 0) || (squash_flag != 0)){
 		size = trace_get_item(&tr_entry); //Fetch next instruction
 	}    
     stall_flag = 0; //Reset stall_flag
@@ -234,7 +405,7 @@ int main(int argc, char **argv)
       t_Addr = tr_entry->Addr;*/
     }  
 	
-	
+	branch_prediction(*tr_entry);
 	/* At this point, we have fetched the next instruction to be enter the pipeline.	
 	 * We must check for hazards that will occur in this cycle before we push the pipeline.
 	*/
@@ -246,18 +417,22 @@ int main(int argc, char **argv)
 	
 // SIMULATION OF A SINGLE CYCLE cpu IS TRIVIAL - EACH INSTRUCTION IS EXECUTED
 // IN ONE CYCLE
-
+	
+	//Cleaning out pipeline 
 	if(empty_flag == 1){
 		empty_count++;
-		if(empty_count >= 6){
-			break;
-		}
+		cycle_number++;
+		
 		//push_pipeline();
 	}
     if (trace_view_on) {// print the executed instruction if trace_view_on=1 
       switch(buff_stages[6].type) {
         case ti_NOP:
-          printf("[cycle %d] NOP:\n",cycle_number) ;
+		  if(squash_print > 2){
+		  printf("[cycle %d] SQUASH:\n", cycle_number);
+		  }else{
+			  printf("[cycle %d] NOP:\n",cycle_number) ;
+		  }
           break;
         case ti_RTYPE:
           printf("[cycle %d] RTYPE:",cycle_number) ;
@@ -292,6 +467,9 @@ int main(int argc, char **argv)
           break;
       }
     }
+	if(empty_count >= 6){
+			break;
+		}
   }
   trace_uninit();
   
